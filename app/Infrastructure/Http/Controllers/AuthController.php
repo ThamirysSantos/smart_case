53c82d7b6762a451c03cae42a11c69b0e3ca8e8c
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2024-05-12 15:16:08 -0300

    created base auth controller and config api base routes

---
+++ app/Infrastructure/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Http\Controllers;
+
+use App\Infrastructure\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    private function __construct()
+    {   
+        
+    }
+
+    public function register(Request $request)
+    {
+
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+
+    public function profile()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Infrastructure\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Open Routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+// Protected Routes
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function() {
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::get('logout', [AuthController::class, 'logout']);
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
