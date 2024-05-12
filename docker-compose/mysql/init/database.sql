b51ee97d4f379ad9292b40b38fac92f3a34c7d27
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2024-05-11 22:30:40 -0300

    updated mysql version

--- docker-compose/mysql/init/database.sql
+++ docker-compose/mysql/init/database.sql
@@ -2,5 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `smart_case`;
 
 -- create local_developer user and grant rights
-CREATE USER 'smart_case'@'db' IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON *.* TO 'smart_case'@'%';
+CREATE USER 'smart_case'@'db' IDENTIFIED WITH mysql_native_password BY 'secret';
+GRANT ALL PRIVILEGES ON .* TO 'smart_case'@'%';
+
+
